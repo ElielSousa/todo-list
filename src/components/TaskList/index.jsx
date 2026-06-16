7075ee189d430a305fbcf4f98f4c2cc5b7724ee6
Eliel <jeliel.nsousa@gmail.com>
2026-06-16 16:59:01 -0300

    feat: implements task filtering based on their completion status

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,19 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskContext } from "../../providers/TaskContext";
 import TaskItem from "./TaskItem";
 
 const TaskList = () => {
   const { taskList } = useContext(TaskContext);
+  const [filter, setFilter] = useState("all");
+
+  const filteredTasks = taskList.filter((task) => {
+    switch (filter) {
+      case "pending":
+        return !task.done;
+      case "completed":
+        return task.done;
+      default:
+        return true;
+    }
+  });
+
   return (
     <>
-      {taskList.length ? (
+      <button onClick={() => setFilter("all")}>Todas</button>
+      <button onClick={() => setFilter("pending")}>Pendentes</button>
+      <button onClick={() => setFilter("completed")}>Concluídas</button>
+      {filteredTasks.length ? (
         <ul>
-          {taskList.map(task => <TaskItem key={task.id} task={task} />)}
+          {filteredTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </ul>
       ) : (
-        <p>Nenhuma tarefa adicionada</p>
+        <p>Nenhuma tarefa aqui...</p>
       )}
-
     </>
   );
 };
